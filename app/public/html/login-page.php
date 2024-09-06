82fceda28144ec7894a5439738838bc5640de49a
benson09121 <benson09.javier@gmail.com>
2024-09-06 16:37:10 +0800

    updated

--- app/public/html/login-page.php
+++ app/public/html/login-page.php
@@ -84,7 +84,7 @@ $(document).ready(function(){
                 $('#ErrorModal').modal('show');
             }
             else if(response == 'success'){
-                window.location.href = './html/home.php';
+                window.location.href = './home.php';
             }
         }, error: function(response){
 
