427c5197d6626e633af0079a068c41550a12ac90
benson09121 <benson09.javier@gmail.com>
2024-09-07 11:30:51 +0800

    Cleaning

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install Docker Compose (if necessary)
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-# Run Docker Compose
-docker-compose build
-docker-compose up
