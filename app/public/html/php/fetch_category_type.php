ff1ef2e9c745034cae0e23e78a8cd68be8701571
benson09121 <benson09.javier@gmail.com>
2024-09-09 21:36:50 +0800

    Commit 21

--- app/public/html/php/fetch_category_type.php
+++ app/public/html/php/fetch_category_type.php
@@ -10,32 +10,5 @@ while($row = $result->fetch_assoc()){
     $category_type[] = $row;
 }
 
-$sql = "SELECT * FROM tbl_minor_violations";
-$result = $conn->query($sql);
-
-$minor_violation = array();
-
-while($row = $result->fetch_assoc()){
-    $minor_violation[] = $row;
-}
-
-$sql = "SELECT * FROM tbl_major_violation";
-$result = $conn->query($sql);
-
-$major_violation = array();
-
-while($row = $result->fetch_assoc()){
-    $major_violation[] = $row;
-}
-
-
-$data = [
-    'category_type' => $category_type,
-    'minor_violation' => $minor_violation,
-    'major_violation' => $major_violation
-];
-
-echo json_encode($data);
-
-$conn->close();
+echo json_encode($category_type);
 ?>
