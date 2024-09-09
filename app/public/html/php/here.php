2332c4b7f21a5f284f8dba6830c1faa3070ea460
benson09121 <benson09.javier@gmail.com>
2024-09-09 21:26:29 +0800

    Commit 19

--- app/public/html/php/here.php
+++ app/public/html/php/here.php
@@ -3,12 +3,12 @@ include '../../database/database_conn.php';
 
 $sql = "SELECT * FROM tbl_major_violation";
 $result = $conn->query($sql);
-
-$major = array();
-
-while($row = $result->fetch_assoc()){
-    $major[] = $row;
+if ($result->num_rows > 0) {
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    echo json_encode($data);
+} else {
+    echo "0 results";
 }
-
-echo json_encode($major);
-?>
