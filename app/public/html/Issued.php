13853eec6dc517658bb51dbb36130b23be392cc0
benson09121 <benson09.javier@gmail.com>
2024-09-17 03:01:36 +0800

    Commit 42

--- app/public/html/Issued.php
+++ app/public/html/Issued.php
@@ -26,7 +26,6 @@ $_SESSION['currentpage'] = "issued";
     <link rel="stylesheet" href="../css/violation-report.css">
     <!-- <link rel="stylesheet" href="../css/studentviol.css"> -->
     <link rel="stylesheet" href="sidenav/sidenav.css">
-    <link rel="stylesheet" href="../css/general.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -57,7 +56,6 @@ $_SESSION['currentpage'] = "issued";
     </div>
 
     <section class="main-do">
-
 
         <div class="body-content">
             <div class="title-page">
