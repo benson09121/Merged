5fdcd66cf99e8148339c72c781d03398baf6d89c
redthecoderist1000 <redochavillo@gmail.com>
2024-10-11 21:55:30 +0800

    student compliance

---
+++ app/public/html/php/getConference.php
@@ -0,0 +1,61 @@
+<?php
+
+// include '../database/database_conn.php';
+// include '../../database/database_conn.php';
+
+
+$sqlconf = "SELECT conf.ref_id, stu.student_id, conf.slip_no, conf.conference_type, conf.scheduled_date
+        FROM sql12729827.tbl_for_conference conf
+        INNER JOIN sql12729827.tbl_major_violation_records rec
+        ON conf.slip_no = rec.slip_no
+        INNER JOIN sql12729827.tbl_student_info stu
+        ON rec.student_id = stu.student_id
+";
+
+
+$resultconf = mysqli_query($conn, $sqlconf);
+$dataConference = [];
+
+foreach ($resultconf as $row) {
+
+    $dataConference[] = [
+
+        'ref_id' => $row['ref_id'],
+        'student_id' => $row['student_id'],
+        'slip_no' => $row['slip_no'],
+        'conference_type' => $row['conference_type'] ?? '',
+        // 'department' => $row['assigned_department'] ?? '--',
+        'scheduled_date' => $row['scheduled_date']
+    ];
+
+}
+
+
+$sqlAttendees = "SELECT * FROM sql12729827.tbl_conf_attendees";
+
+$resultAttendees = mysqli_query($conn, $sqlAttendees);
+$dataAttendees = [];
+
+foreach ($resultAttendees as $row) {
+
+    $dataAttendees[] = [
+
+        'conf_no' => $row['conference_no'],
+        'name' => $row['name']
+
+    ];
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+mysqli_close($conn);
--- app/public/html/php/getIntervention.php
+++ app/public/html/php/getIntervention.php
@@ -1,8 +1,4 @@
 <?php
-// header('Content-type: application/json; charset=utf-8');
-
-// include('../../database/database_conn.php');
-
 
 $sql = "SELECT intervention.ref_id, student.student_id, violation.slip_no, intervention.method, intervention.assigned_department, intervention.compliance_due_date
         FROM sql12729827.tbl_for_intervention as intervention
@@ -12,48 +8,23 @@ $sql = "SELECT intervention.ref_id, student.student_id, violation.slip_no, inter
         ON student.student_id = violation.student_id
 ";
 
-$output = array();
 
 $result = mysqli_query($conn, $sql);
 $data = array();
-$filtered_row = $result->num_rows;
 
 foreach ($result as $row) {
 
     $data[] = [
-        // 'DT_RowID' => $row['ref_id'],
-        // 'DT_RowData' => ['pkey' => $row['ref_id']],
 
         'ref_id' => $row['ref_id'],
         'student_id' => $row['student_id'],
         'slip_no' => $row['slip_no'],
         'method' => $row['method'],
-        'department' => $row['assigned_department'],
+        'department' => $row['assigned_department'] ?? '--',
         'due_date' => $row['compliance_due_date']
-
-        // $row['ref_id'],
-        // $row['student_id'],
-        // $row['slip_no'],
-        // $row['method'],
-        // $row['assigned_department'],
-        // $row['compliance_due_date']
     ];
 
 }
 
 
-$output = array(
-    // 'filtered_row' => $filtered_row,
-    "draw" => 1,
-    "recordsTotal" => $filtered_row,
-    "recordsFiltered" => $filtered_row,
-    'data' => $data
-);
-
-// header("HTTP/1.0 200 goods");
-// echo json_encode(['status' => 'Success', 'message' => 'Pick your poison', 'data' => $data]);
-
-// echo json_encode($data);
-// echo json_encode($output);
-
-mysqli_close($conn);
+// mysqli_close($conn);
