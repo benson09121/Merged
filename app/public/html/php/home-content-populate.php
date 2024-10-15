13727adb1a3012fef977d90e2b43974ec2765887
redthecoderist1000 <redochavillo@gmail.com>
2024-10-15 11:33:26 +0800

    analytics_dashboard

--- app/public/html/php/home-content-populate.php
+++ app/public/html/php/home-content-populate.php
@@ -1,7 +1,19 @@
 <?php
 
 
-$sql = 'SELECT * FROM tbl_school_info;';
+$sql = 'SELECT sch.school_id, sch.school_name, sch.description, count(rec.slip_no) as majCount, count(min.slip_no) as minCount
+            FROM sql12729827.tbl_school_info as sch
+            LEFT JOIN sql12729827.tbl_department_info as dep
+            ON sch.school_id = dep.school_id
+            LEFT JOIN sql12729827.tbl_course_info as cor
+            ON cor.department_id = dep.department_id
+            LEFT JOIN sql12729827.tbl_student_info as stu
+            ON stu.course_id = cor.course_id
+            LEFT JOIN sql12729827.tbl_major_violation_records as rec
+            ON stu.student_id = rec.student_id
+            LEFT JOIN sql12729827.tbl_minor_violation_records as min
+            ON stu.student_id = min.student_id
+            GROUP BY school_id;';
 
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
@@ -10,7 +22,7 @@ if (mysqli_num_rows($result) > 0) {
         ?>
         <div class="dept dept1">
             <div class="card">
-                <div class="card-header">-</div>
+                <div class="card-header"><?php echo $row['majCount'] + $row['minCount']; ?></div>
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $row['school_name']; ?></h5>
                     <p class="card-text"><?php echo $row['description']; ?></p>
--- app/public/html/php/home-content.php
+++ app/public/html/php/home-content.php
@@ -5,13 +5,36 @@ $id = $_POST['id'];
 
 
 
-if($id == 0){
-    $sql = "SELECT * FROM tbl_school_info";
+if ($id == 0) {
+    $sql = "SELECT sch.school_id, sch.school_name, sch.description, count(rec.slip_no) as majCount, count(min.slip_no) as minCount
+            FROM sql12729827.tbl_school_info as sch
+            LEFT JOIN sql12729827.tbl_department_info as dep
+            ON sch.school_id = dep.school_id
+            LEFT JOIN sql12729827.tbl_course_info as cor
+            ON cor.department_id = dep.department_id
+            LEFT JOIN sql12729827.tbl_student_info as stu
+            ON stu.course_id = cor.course_id
+            LEFT JOIN sql12729827.tbl_major_violation_records as rec
+            ON stu.student_id = rec.student_id
+            LEFT JOIN sql12729827.tbl_minor_violation_records as min
+            ON stu.student_id = min.student_id
+            GROUP BY school_id";
     $result = mysqli_query($conn, $sql);
     $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
     echo json_encode($rows);
-}else{
-    $sql = "SELECT * FROM tbl_department_info WHERE school_id='$id'";
+} else {
+    $sql = "SELECT dep.department_id, dep.school_id, dep.name, dep.description, count(maj.slip_no) as majCount, count(min.slip_no) as minCount
+            FROM sql12729827.tbl_department_info as dep
+            LEFT JOIN sql12729827.tbl_course_info as cor
+            ON cor.department_id = dep.department_id
+            LEFT JOIN sql12729827.tbl_student_info as stu
+            ON cor.course_id = stu.course_id
+            LEFT JOIN sql12729827.tbl_major_violation_records as maj
+            ON stu.student_id = maj.student_id
+            LEFT JOIN sql12729827.tbl_minor_violation_records as min
+            ON stu.student_id = min.student_id
+            WHERE school_id='$id'
+            GROUP BY department_id";
     $result = mysqli_query($conn, $sql);
     $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
     echo json_encode($rows);
